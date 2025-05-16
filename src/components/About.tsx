52c9616abad347989d82dcb70e375c10580d5d33
SAADAT <134273510+NOXUSTIC@users.noreply.github.com>
2025-05-17 05:13:48 +0600

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -33,7 +33,7 @@ const About = () => {
           <Card className="transform transition-all hover:scale-105 duration-300">
             <CardContent className="p-6">
               <h3 className="text-xl font-bold mb-4 flex items-center">
-                <svg className="w-6 h-6 mr-2 text-[#3A0C3A]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <svg className="w-6 h-6 mr-2 text-[#30A5FF]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"></path>
                 </svg>
                 My Mission
