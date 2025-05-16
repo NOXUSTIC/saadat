756d774eaa96501b4634e552b864e49ea3f32a12
SAADAT <134273510+NOXUSTIC@users.noreply.github.com>
2025-05-17 05:18:32 +0600

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -64,7 +64,7 @@ const About = () => {
           <Card className="transform transition-all hover:scale-105 duration-300" style={{animationDelay: "0.4s"}}>
             <CardContent className="p-6">
               <h3 className="text-xl font-bold mb-4 flex items-center">
-                <svg className="w-6 h-6 mr-2 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <svg className="w-6 h-6 mr-2 text-[#30A5FF]" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
                 </svg>
                 Continuous Learning
