bf20a45093185e9a727395ebbedcaf28bc2fd4e9
SAADAT <134273510+NOXUSTIC@users.noreply.github.com>
2025-06-16 01:00:27 +0600

    Update ModelDetail.tsx

--- src/pages/ModelDetail.tsx
+++ src/pages/ModelDetail.tsx
@@ -11,7 +11,7 @@ const ModelDetail = () => {
   const { id } = useParams();
   
   // Use the STEP file that exists in the public folder
-  const modelPath = "/models/LFR/LFR.3MF";
+  const modelPath = "public/models/LFR.3MF";
 
   return (
     <div className="min-h-screen">
